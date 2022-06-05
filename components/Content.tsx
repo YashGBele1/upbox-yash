51713493f39776a178582456200ea1a697e7a80b
YashGBele1 <eastonlinuxpoi@gmail.com>
2022-06-05 01:36:26 -0700

    commit

--- components/Content.tsx
+++ components/Content.tsx
@@ -38,7 +38,7 @@ function Content() {
 
       <p className="px-7 md:text-center text-sm sm:text-xl mb-10 font-semibold text-gray-500">Your plan auto-renews at the end of 30 days from the 1st day you signed-up.</p>
 
-      <section className="flex justify-center mt-20 mb-5">
+      <section className="flex justify-center mt-10 mb-5">
         <div className="grid grid-cols-1 justify-items-center gap-1 pb-7">
           <h1 className="mt-10 text-4xl md:text-5xl lg:text-6xl font-serif">How it works?</h1>
           <img src="/images/UNDERLINE.svg" />
@@ -65,13 +65,44 @@ function Content() {
         </div>
       </section>
 
-      <section className="sm:flex justify-center space-x-10 pb-20">
+      <section className="sm:flex justify-center space-x-10 pb-10">
         <div>
           <img src="/images/PERSON2.png" />
         </div>
         <div className='space-y-5 py-10 pr-7'>
           <h1 className='max-w-lg text-3xl sm:text-4xl font-serif'>Try it on at home</h1>
           <h2 className="max-w-sm text-sm sm:text-lg font-semibold text-gray-500">Your box will arrive within 3-5 days (usually sooner) and you get the joy to unbox your Upbox. Try it all on and send back whatever you’re not in love with.</h2>
+        </div>
+      </section>
+
+      <section className="grid justify-center mb-20">
+        <button className="hover:scale-105 text-xs sm:text-sm font-bold tracking-widest border-4 border-[#EF8094] rounded-3xl px-8 py-3 uppercase">try it for yourself</button>
+      </section>
+
+      <section className="bg-[#F5F8FF] py-16">
+        <h1 className="text-center text-4xl sm:text-5xl font-serif sm:font-normal mb-10">Social Posts</h1>
+
+        <div className="px-10 sm:px-24 grid justify-items-center sm:grid-cols-2 lg:grid-cols-4 gap-10">
+          <div className="grid justify-items-start gap-1 pb-3">
+            <img src="/images/img1.svg" />
+            <p className="text-sm sm:text-lg font-semibold text-gray-500">How to rock the lip look that turns heads</p>
+          </div>
+          <div className="grid justify-items-start gap-1 pb-3">
+            <img src="/images/img2.svg" />
+            <p className="text-sm sm:text-lg font-semibold text-gray-500">Find the perfect shade for the season</p>
+          </div>
+          <div className="grid justify-items-start gap-1 pb-3">
+            <img src="/images/img3.svg" />
+            <p className="text-sm sm:text-lg font-semibold text-gray-500">The 5 eye shadow secrets you never knew</p>
+          </div>
+          <div className="grid justify-items-start gap-1 pb-3">
+            <img src="/images/img4.svg" />
+            <p className="text-sm sm:text-lg font-semibold text-gray-500">The pro-tips for at home hair dying</p>
+          </div>
+        </div>
+
+        <div className="grid justify-center mt-14 sm:mt-20">
+          <button className="hover:scale-105 text-xs sm:text-sm font-bold tracking-widest text-white bg-[#EF8094] rounded-3xl px-8 py-3 uppercase">follow us on instagram</button>
         </div>
       </section>
 
